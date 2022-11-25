cdf413f05b397847b7cd947c935f6150cbe64948
camilalonero <lonerocamila@gmail.com>
2022-11-25 17:31:03 -0300

    funcion para resultado

--- app.js
+++ app.js
@@ -11,37 +11,38 @@ while (seleccionarProductos !="0"){
     switch(seleccionarProductos){
         case 1:
               seleccionarCantidad = Number(prompt ("el producto seleccionado es Taza, indique la cantidad"))
-                 total+= cantidad (seleccionarCantidad , $30)
+                 
         break;
 
         case 2:
              seleccionarCantidad= Number(prompt ("el producto seleccionado es Vaso, indique la cantidad"))
-                 total+= cantidad (seleccionarCantidad , $150)
+                 
         break;
 
         case 3:
              seleccionarCantidad = Number(prompt ("el producto seleccionado es Kindle, indique la cantidad"))
-                 total+= cantidad (seleccionarCantidad , $50000)
+                 
         break;
         case 4:
              seleccionarCantidad = Number(prompt ("el producto seleccionado es Buda, indique la cantidad"))
-                 total+= cantidad (seleccionarCantidad , $6000)          
+                     
         break; 
     default:
              break;
     }
-        seleccionarProductos = Number(prompt("1-Taza $30 2-Vaso $150 3-Kindle $50000 4-Buda $6000"))
+        seleccionarProductos = Number(prompt("1-Taza $30 2-Vaso $150 3-Kindle $50000 4-Buda $6000"));
           
+    
+     
+    cantidad =(cantidad,  precio) => {
+        let resultado= cantidad * precio
+        return resultado
     }
-
     //Le hacemos saber el costo total de la compra con un alert
-    alert ("el total de la compra es de " + total )
+    alert ("el total de la compra es de " + resultado )
 
 
-    /* para que el cliente pueda calcular el total, recurrimos a una funcion
-    const cantidad =(cantidad, precio) => {
+}
 
-        return cantidad * precio
-
-    }
-   */
+    
+ 
